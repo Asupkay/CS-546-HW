362d3adb8c7622cfb143fb1aeffbece1882b7737
Alex Supkay <asupkay1124@gmail.com>
2017-09-12 16:15:58 -0400

    Finish triangle function

--- Lab2/index.js
+++ Lab2/index.js
@@ -1,7 +1,17 @@
 //index.js
+//*-------------------------------------------------------*
+//| Name: index.js                                        |
+//| Description: print 10 triangles, 10 squares, and 10   |
+//| rhombi of sizes of your choosing                      |
+//| Author: Alex Supkay                                   |
+//| Pledge: I pledge my honor that I have abided by the   |
+//| Stevens Honor System.                                 |
+//*-------------------------------------------------------*
+
+//Import my printshape module
 const shapePrinter = require("./printShape.js");
 
-shapePrinter.testCommand(10);
+shapePrinter.square(10);
 
 //Print triangles of size ranging from 1 to 10
 console.log("-------------------------------");
@@ -9,7 +19,7 @@ console.log("|          Triangle           |");
 console.log("-------------------------------");
 for(let triangleSize = 1; triangleSize <= 10; triangleSize++) {
     console.log(`--Triangle of Size ${triangleSize}--`);
-    //shapePrinter.triangle(triangleSize);
+    shapePrinter.triangle(triangleSize);
 }
 
 //Print squares ranging from 1 to 10
--- Lab2/printShape.js
+++ Lab2/printShape.js
@@ -1,7 +1,40 @@
-//printShape.js
+//*-------------------------------------------------------*
+//| Name: printShape.js                                   |
+//| Description: provide several ways of printing shapes, |
+//| each of a certain number of sizes                     |
+//| Author: Alex Supkay                                   |
+//| Pledge: I pledge my honor that I have abided by the   |
+//| Stevens Honor System.                                 |
+//*-------------------------------------------------------*
 
 module.exports = {
-    testCommand: function(number){
-        console.log(number);
+    triangle: function(lines) {
+        if(!(lines > 0) && lines !== 'number') {
+            throw "Input must be a number more than 1";
+        }
+        triangle = "";
+        for(let row = 0; row < lines; row++) {
+            for(let cspace = lines - row; cspace > 0; cspace--) {
+                triangle += " ";
+            }
+            triangle += "/";
+            if(row == lines-1) {
+                for(let spaces = row*2; spaces > 0; spaces--) {
+                    triangle += "-";
+                }
+            } else {
+                for(let spaces = row*2; spaces > 0; spaces--) {
+                    triangle += " ";
+                }
+            }
+            triangle += "\\\n";
+        }
+        console.log(triangle);
+    },
+    square: function(lines) {
+        console.log(lines);
+    },
+    rhombus: function(lines) {
+        console.log(lines);
     }
 };
