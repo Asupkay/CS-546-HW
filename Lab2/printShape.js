9d460a8341ba7b6eb1fa4041d5ae538c9640b462
Alex Supkay <asupkay1124@gmail.com>
2017-09-12 16:52:12 -0400

    Add error checking and comments of triangle

--- Lab2/printShape.js
+++ Lab2/printShape.js
@@ -11,7 +11,7 @@ module.exports = {
     triangle: function(lines) {
         //Check that the input is valid
         if(!(lines > 0) && lines !== 'number') {
-            throw "Input must be a number more than 1";
+            throw "Input must be a number more than 0";
         }
         //This will be the variable where the string is construted
         triangle = "";
@@ -40,9 +40,17 @@ module.exports = {
         console.log(triangle);
     },
     square: function(lines) {
+        //Check that the input is valid
+        if(!(lines > 2) && lines !== 'number') {
+            throw "Input must be a number more than 2";
+        }
+        //String for holding the constructed string
         square = "";
+        //Construct each row
         for(let row = 0; row < lines; row++) {
+            //Start the row
             square += "|";
+            //If it is the first or last row use - if not use spaces
             if(row == 0 || row == lines-1) {
                 for(let column = 0; column < lines; column++) {
                     square += '-';
@@ -52,8 +60,10 @@ module.exports = {
                     square += ' ';
                 }
             }
+            //End the row and make a new line
             square += "|\n";
         }
+        //print the triangle
         console.log(square);
     },
     rhombus: function(lines) {
