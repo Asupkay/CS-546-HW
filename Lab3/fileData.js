3343bc9ffafcbea24c3509843c426794186f5ed3
Alex Supkay <asupkay1124@gmail.com>
2017-09-20 17:43:09 -0400

    Add error checking polish general.

--- Lab3/fileData.js
+++ Lab3/fileData.js
@@ -12,7 +12,7 @@ const fs = bluebird.promisifyAll(require("fs"));
 
 //Get the file as a string
 module.exports.getFileAsString = async function getFileAsString(path) {
-    if(!path) throw "You must provide a path";
+    if(!path || typeof path != 'string') throw "You must provide a path and it must be a string";
     
     try { 
         let content = await fs.readFileAsync(path, "utf8");
@@ -24,7 +24,7 @@ module.exports.getFileAsString = async function getFileAsString(path) {
 
 //Get the file as a json object
 module.exports.getFileAsJSON = async function getFileAsJSON(path) {
-    if(!path) throw "You must provide a path";
+    if(!path || typeof path != 'string') throw "You must provide a path and it must be a string";
     try { 
         let content = await fs.readFileAsync(path, "utf8");
         return JSON.parse(content);
@@ -35,7 +35,7 @@ module.exports.getFileAsJSON = async function getFileAsJSON(path) {
 
 //Save file as a string to file
 module.exports.saveStringToFile = async function saveStringToFile(path, text) {
-    if(!path || !text) throw "You must provide a path and text";
+    if(!path || !text || typeof path != 'string' || typeof text != 'string') throw "You must provide a path and text and both must be strings";
 
     try {
         await fs.writeFileAsync(path, text);
@@ -47,7 +47,7 @@ module.exports.saveStringToFile = async function saveStringToFile(path, text) {
 
 //SAve file as a string to file
 module.exports.saveJSONToFile = async function saveJSONToFile(path, obj) {
-    if(!path || !obj) throw "You must provide a path and obj";
+    if(!path || !obj || typeof path != 'string' || typeof obj != 'object') throw "You must provide a path and obj the path must be a string and the object must be a JSON object";
 
     try {
         await fs.writeFileAsync(path, JSON.stringify(obj, null, 4));
--- Lab3/textMetrics.js
+++ Lab3/textMetrics.js
@@ -8,6 +8,8 @@
 
 //Simplifies the text by putting it to lower case, removing any symbols, and turning any whitspace to one space
 module.exports.simplify = function (text) {
+    if(!text || typeof text != 'string') throw "text for simplify must be a string and exist";
+
     let simplifiedText = text.toLowerCase();
     simplifiedText = simplifiedText.replace(/[^\s\w]/g, '');
     simplifiedText = simplifiedText.replace(/\s/g, ' ');
@@ -17,6 +19,8 @@ module.exports.simplify = function (text) {
 
 //Compiled together the metrics into a JSON object and returns it
 module.exports.createMetrics = function (text) {
+    if(!text || typeof text != 'string') throw "text for createMetrics must be a string and exist";
+
     let simplifiedText = module.exports.simplify(text);
     let totalLetters = simplCountLetters(simplifiedText);
     let totalWords = simplCountWords(simplifiedText);
