7515fc511ef62582d86d55c938c748ff2de20eb8
Alex Supkay <asupkay1124@gmail.com>
2017-09-22 14:30:47 -0400

    switched from .then() to await

--- Lab3/app.js
+++ Lab3/app.js
@@ -15,52 +15,52 @@ async function processFile(path) {
     if(path && typeof path == 'string') {
         let resultPath = path + ".result.json";
         //See if there is already a results file for the text file
-        fileData.getFileAsJSON(resultPath).then((contents) => {
+        try {
+            let contents = await fileData.getFileAsJSON(resultPath);
             console.log(resultPath + " was found:");
             console.log(contents);    
-        }).catch((error) => {
+        } catch(error) {
             //If there was not a result file or it wasn't in JSON attempt to create one
             console.log(error.message);
             console.log(resultPath + " was not found or was not a JSON object. Attempting to create.");
             //Try to get the txt file of the path
-            fileData.getFileAsString(path + ".txt").then((contents) => {
+            try {
+                let contents = await fileData.getFileAsString(path + ".txt");
                 console.log(path + ".txt found. Attempting to create debug simplify file of it.");
                 //Save the simplified string to debug for troubleshooting
-                fileData.saveStringToFile(path + ".debug.txt", textMetrics.simplify(contents)).then((success) => {
+                try {
+                    await fileData.saveStringToFile(path + ".debug.txt", textMetrics.simplify(contents))
                     console.log(path + ".debug.txt created. Attempting to create metrics and store in file.");
                     let textInfo = textMetrics.createMetrics(contents);
                     //Try to put the metric data into a file
-                    fileData.saveJSONToFile(path + ".result.json", textInfo).then((success) => {
+                    try {
+                        await fileData.saveJSONToFile(path + ".result.json", textInfo);
                         console.log(path + ".result.json created. Here it is: "); 
                         console.log(textInfo);
-                    }).catch((error) => {
+                    } catch(error) {
                         console.log(error.message);
                         console.log("Error on saving JSON to file " + path + ".result.json");
-                    });
-                }).catch((error) => {
-                    console.log(error.message);
-                    console.log("Error on saving String to file " + path + ".debug.txt");
-                });
-            }).catch((error) => {
+                    }
+                } catch(error) {
+                console.log(error.message);
+                console.log("Error on saving String to file " + path + ".debug.txt");
+                }
+            } catch(error) {
                 console.log(error.message);
                 console.log("Error on getting file as String " + path + ".txt");
-            });
-        });
+            }
+        }
     } else {
         console.log("Path must exist and be a string.");
     }
 }
 
-try {
-    //processFile();
-    //processFile("Cahpter");
-    processFile("chapter1");
-    processFile("chapter2");
-    processFile("chapter3");
-    //processFile(3);
-    console.log("===========Text Metrics Example================="); 
-    console.log(textMetrics.createMetrics("Hello, my -! This is a great day to say hello.\n\n\tHello! 2 3 4 23"));
-    console.log("================================================"); 
-} catch (error) {
-    console.log(error.stack);
-}
+//processFile();
+//processFile("Cahpter");
+processFile("chapter1");
+processFile("chapter2");
+processFile("chapter3");
+//processFile(3);
+console.log("===========Text Metrics Example================="); 
+console.log(textMetrics.createMetrics("Hello, my -! This is a great day to say hello.\n\n\tHello! 2 3 4 23"));
+console.log("================================================"); 
