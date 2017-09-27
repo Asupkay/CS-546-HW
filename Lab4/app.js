a20de034e789d49464cb7ceb477ddcdbfc9eca79
Alex Supkay <asupkay1124@gmail.com>
2017-09-27 14:22:32 -0400

    Create mongoConnection, mongo Collections and set up backbone of todo.js

--- Lab4/app.js
+++ Lab4/app.js
@@ -1,1 +1,1 @@
-here
+console.log("HERE");
---
+++ Lab4/todo.js
@@ -0,0 +1,17 @@
+module.exports = {
+    async function createTask(title, description) {
+
+    },
+    async function getAllTasks() {
+
+    }, 
+    async function getTask(id) {
+
+    },
+    async function completeTask(taskId) {
+
+    },
+    async function removeTask(id) {
+
+    }
+}
