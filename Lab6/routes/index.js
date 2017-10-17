05a60279132503ae52c93d2e7589ab7a2c430c57
Alex Supkay <asupkay1124@gmail.com>
2017-10-17 16:27:26 -0400

    Complete app.js and routes/index.js

--- Lab6/routes/index.js
+++ Lab6/routes/index.js
@@ -1,1 +1,15 @@
-//initialize
+const aboutRoutes = require("./about");
+const educationRoutes = require("./education");
+const storyRoutes = require("./story");
+
+const constructorMethod = (app) => {
+    //app.use("/about", aboutRoutes);
+    //app.use("/education", educationRoutes);
+    //app.use("/story", storyRoutes);
+
+    app.use("*", (req, res) => {
+        res.status(404).json({error: "Not Found"});
+    });
+};
+
+module.exports = constructorMethod;
