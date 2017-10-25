b4e6544373abd2f121d689377bc0faf69eacc6ee
Alex Supkay <asupkay1124@gmail.com>
2017-10-24 20:57:47 -0400

    start data/recipes.js

--- Lab7/data/recipes.js
+++ Lab7/data/recipes.js
@@ -1,1 +1,26 @@
-//init
+const mongoCollections = require("../config/mongoCollections");
+const recipes = mongoCollections.posts;
+const uuid = require("node-uuid");
+
+const exportedMethods {
+    async getAllPosts() {
+        recipeCollection = await recipes();
+        return await recipeCollection.find({}).toArray();
+    }
+    
+    async getAllIDAndTitlePosts() {
+        recipeCollection = await recipes();
+        return await recipeCollection.find({}, {id: 1, title: 1}).toArray();
+    }
+
+    async getPostByID (id) {
+        if (!id) throw "No ID provided";
+        recipeCollection = await recipes();
+        recipe = await recipeCollection.fint({_id: id});
+
+        if (!recipe) throw "Recipe not found";
+        return recipe;
+    }
+}
+
+module.exports = exportedMethods;
