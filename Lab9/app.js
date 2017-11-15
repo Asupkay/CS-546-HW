448e0c7974b485c5b514897376da33383fcefb62
Alex Supkay <asupkay1124@gmail.com>
2017-11-15 00:37:27 -0500

    Have all routes set up but not hitting the authentication correctly

--- Lab9/app.js
+++ Lab9/app.js
@@ -1,12 +1,57 @@
 const configRoutes = require("./routes");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const data = require("./data");
+const bcrypt = require('bcrypt-nodejs');
+
 const express = require("express");
 const app = express();
+
+const static = express.static(__dirname + '/public');
+const exphbs = require('express-handlebars');
+
+app.use("/public", static);
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 var passport = require('passport')
     , LocalStrategy = require('passport-local').Strategy;
 
-app.use(bodyParser.json());
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+
+        let user;
+        try {
+            user = data.findUserByUsername(username);
+        } catch (e) {
+            return done(null, false, {message: 'Incorrect username.'});
+        }
+        
+        bcrypt.compare(password, user.hashedPassword, (err, res) => {
+            if(res === true) {
+                return done(null, user);
+            } else {
+                return done(null, false, {message: 'Incorrect password.'});
+            }
+        });
+    }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    let user = data.findUserById(id);
+    done(null, user);
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 configRoutes(app);
 
 app.listen(3000, () => {
--- Lab9/routes/index.js
+++ Lab9/routes/index.js
@@ -1,13 +1,16 @@
+const path = require("path")
+
 const loginRoutes = require("./login");
 const privateRoutes = require("./private");
 
 const constructorMethod = (app) => {
-    app.get("/", (req, res) => {
-        res.json({Nice: "you hit me"});
-    });
-
     app.use("/login", loginRoutes);
     app.use("/private", privateRoutes);
+
+    app.get("/", (req, res) => {
+        res.render("loginpage/login");
+    });
+
     app.use("*", (req, res) => {
         res.sendStatus(404);
     });
